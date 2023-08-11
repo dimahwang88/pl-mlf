49db2efdf38574dd8828a3f0ae79b1366ce9f011
Dmitriy Khvan <dmitriy.khvan@bepro11.com>
2023-08-11 17:02:39 +0200

    adding load checkpoint example

---
+++ autoencoder_deploy.py
@@ -0,0 +1,79 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+import lightning.pytorch as pl
+
+from lightning.pytorch.cli import LightningCLI
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+from lightning.pytorch.loggers import MLFlowLogger
+import mlflow
+
+from argparse import ArgumentParser
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(28 * 28, 64), nn.ReLU(), nn.Linear(64, 3))
+
+    def forward(self, x):
+        return self.l1(x)
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.l1 = nn.Sequential(nn.Linear(3, 64), nn.ReLU(), nn.Linear(64, 28 * 28))
+
+    def forward(self, x):
+        return self.l1(x)
+    
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+        self.save_hyperparameters()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = F.mse_loss(x_hat, x)
+        self.logger.experiment.log_metric(run_id=self.logger.run_id, key="train_loss", value=loss)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        val_loss = F.mse_loss(x_hat, x)
+        self.logger.experiment.log_metric(run_id=self.logger.run_id, key="val_loss", value=val_loss)
+    
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        test_loss = F.mse_loss(x_hat, x)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+    
+    def forward(self, x):
+        return x
+
+if __name__ == "__main__":
+    model = LitAutoEncoder.load_from_checkpoint("/media/data/pl-mlf/checkpoints/lightning_logs/version_0/checkpoints/epoch=0-step=48000.ckpt")
+    model.eval()
+    x = torch.randn(1, 64)
+
+    with torch.no_grad():
+        y_hat = model(x)
+        print (y_hat)
